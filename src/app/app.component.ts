300ad54a1cc79e46f9156a4e6ee11cb29c216530
Stanislaw <staskargapolcev26@gmail.com>
2024-11-24 15:23:48 +0300

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-
 import { RouterOutlet } from '@angular/router';
-
 import { HeaderComponent } from './header/header.component';
-
+import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
